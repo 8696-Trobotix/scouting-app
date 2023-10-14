a764fe6dd7e885c707afa985d0b7f6632fc2dbf5
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-10-14 19:18:45 +0000

    Started app home layout.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,221 +1,72 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  print('Testing!');
-  runApp(MyApp());
+  runApp(ScoutingApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ScoutingApp extends StatelessWidget {
+  const ScoutingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+      create: (context) => ScoutingAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Scouting App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         ),
-        home: MyHomePage(),
+        home: HomePage(),
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-  var favorites = <WordPair>[];
+class ScoutingAppState extends ChangeNotifier {
+  var teams = <Team>[];
+  var ids = <int?>{};
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-    notifyListeners();
-  }
-
-  void unfavorite(WordPair pair) {
-    favorites.remove(pair);
-    notifyListeners();
+  void testTeam() {
+    Team test = Team(name: 'Test Team', description: 'Placeholder', id: 3000);
+    teams.add(test);
+    ids.add(test.id);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+class Team {
+  String name;
+  int? id, rank, gears;
+  bool hasTeleOp, hasAutonomous, graciouslyProfessional;
+  String description;
+  List<Image>? images;
 
-class _MyHomePageState extends State<MyHomePage> {
-
-  var selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      default:
-        throw UnimplementedError('No widget for $selectedIndex.');
-    }
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600,
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                    // print('selected: $value');
-                  },
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
-              ),
-            ],
-          ),
-        );
-      }
-    );
-  }
-}
-
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-            '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-            onTap: () {
-              appState.unfavorite(pair);
-            },
-          ),
-      ],
-    );
-  }
-}
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
+  Team({
+    this.name = '', 
+    this.id, this.rank, this.gears, 
+    this.hasTeleOp = true, this.hasAutonomous = false, this.graciouslyProfessional = true, 
+    this.description = '', 
+    this.images
   });
+}
 
-  final WordPair pair;
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
+    var appState = context.watch<ScoutingAppState>();
 
-    return Card(
-      color: theme.colorScheme.primary,
-      elevation: 20.0,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Text(
-          pair.asLowerCase,
-          style: style,
-          semanticsLabel: "${pair.first} ${pair.second}",
-        ),
+    appState.testTeam();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scouting App')
+      ),
+      body: ListView(
+        children: [
+          for (Team team in appState.teams)
+            Text(team.name + team.description)
+        ],
       ),
     );
   }
